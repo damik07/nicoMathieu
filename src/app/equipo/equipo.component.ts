4dd97338b7d6a8dd954431deeb05adb34f91171e
damik07 <96784561+damik07@users.noreply.github.com>
2023-07-18 16:41:03 -0300

    2 files modified

--- src/app/equipo/equipo.component.ts
+++ src/app/equipo/equipo.component.ts
@@ -8,21 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class EquipoComponent implements OnInit {
 
   habilidadesList:any= [
-    {id: 1, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/1.PNG"},
-    {id: 2, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/2.PNG"},
-    {id: 3, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/3.PNG"},
-    {id: 4, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/4.PNG"},
-    {id: 5, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/5.PNG"},
-    {id: 6, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/6.PNG"},
-    {id: 7, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/7.PNG"},
-    {id: 8, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/8.PNG"},
-    {id: 9, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/9.PNG"},
-    {id: 10, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/10.PNG"},
-    {id: 11, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/11.PNG"},
-    {id: 12, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/12.PNG"},
-    {id: 13, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/13.PNG"},
-    {id: 14, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/14.PNG"},
-    {id: 15, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/15.PNG"}
+    {id: 1, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/1.png"},
+    {id: 2, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/2.png"},
+    {id: 3, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/3.png"},
+    {id: 4, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/4.png"},
+    {id: 5, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/5.png"},
+    {id: 6, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/6.png"},
+    {id: 7, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/7.png"},
+    {id: 8, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/8.png"},
+    {id: 9, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/9.png"},
+    {id: 10, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/10.png"},
+    {id: 11, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/11.png"},
+    {id: 12, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/12.png"},
+    {id: 13, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/13.png"},
+    {id: 14, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/14.png"},
+    {id: 15, src:"https://cdn.jsdelivr.net/gh/damik07/nicoMathieu@main/src/assets/img/15.png"}
   ];
 
   constructor() { }
